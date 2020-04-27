040c168bcd30fc4f36a58a73da1c172b07ad851e
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-04-27 16:01:46 -0600

    Sugerencia Willi modelo Institution

--- tutos/institutions/models.py
+++ tutos/institutions/models.py
@@ -11,14 +11,14 @@ class Institution(models.Model):
 
 class Career(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    institution = models.ForeignKey('institutions.Institution', on_delete=models.SET_NULL)
+    institution = models.ForeignKey('institutions.Institution', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Course(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    institution = models.ForeignKey('institutions.Institution', on_delete=models.SET_NULL)
+    institution = models.ForeignKey('institutions.Institution', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
