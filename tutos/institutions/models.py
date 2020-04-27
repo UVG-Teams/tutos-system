dea858f0c957c15adaccfcb7c5e817629906b4a7
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-04-27 15:39:17 -0600

    ?

--- tutos/institutions/models.py
+++ tutos/institutions/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Institution(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
-    institution_type = models.CharField(max_length=50, null=False, blank=False)
-    location = models.ForeignKey('location.Location', on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return self.name
