ecd113b850dd144dfd67a9a05b0cb776eaac2a8a
marcofuentes05 <marcofuentes1607@gmail.com>
2020-04-27 16:02:25 -0600

    Correccion v2

--- tutos/tutorias/models.py
+++ tutos/tutorias/models.py
@@ -6,7 +6,7 @@ class Tutor(models.Model):
     score = models.DecimalField(max_digits = 5, decimal_places = 2)
     description = models.CharField(max_length = 120, null = True)
     hours_done = models.PositiveIntegerField()
-    idividual_price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    individual_price = models.DecimalField(max_digits = 6, decimal_places = 2)
     grupal_price = models.DecimalField(max_digits=6, decimal_places=2)
     user = models.ForeignKey(
         'django.contrib.auth.user',
