0aa31ca967bf79e9a09e45e646f4b984035d8179
marcofuentes05 <marcofuentes1607@gmail.com>
2020-04-28 09:21:17 -0600

    Modelo tutoria creado

--- tutos/tutorias/models.py
+++ tutos/tutorias/models.py
@@ -13,3 +13,26 @@ class Tutor(models.Model):
         null = False,
         on_delete=models.CASCADE
     )
+
+class Tutoria(models.Model):
+    tutor = models.ForeignKey(
+        'tutorias.Tutor',
+        null = False,
+        on_delete = models.CASCADE
+    )
+    tutorado = modelos.ForeignKey(
+        'django.contrib.auth.user',
+        null = False,
+        on_delete = models.SET_NULL
+    )
+    location = modelos.ForeignKey(
+        'locations.location',
+        null=False,
+        on_delete=models.SET_NULL
+    )
+    status = models.PositiveIntegerField()
+    topic = models.PositiveIntegerField() #TODO ver si es FK hacia modelo topic
+    course = models.PositiveIntegerField() #TODO ver si es FK hacia modelo course
+    datetime = models.TimeField(auto_now=False, auto_now_add=False)
+    hours = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits = 6, decimal_places = 2)
