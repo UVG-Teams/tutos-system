7d8a26d353cbfbe09cfe83997f28c2b61eb09c2e
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-04-28 19:00:31 -0600

    Subject Model

--- tutos/subjects/models.py
+++ tutos/subjects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
