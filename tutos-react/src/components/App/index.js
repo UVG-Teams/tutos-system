700b3308ceeee8910e48c5fdddccfbbbaa48e897
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-05-03 21:53:48 -0600

    Navbar

--- tutos-react/src/components/App/index.js
+++ tutos-react/src/components/App/index.js
@@ -3,13 +3,15 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { configureStore } from '../../store';
 
+import Index from '../Index';
+
 const store = configureStore();
 
 const App = () => (
     <Provider store = {store}>
         <Router>
             <Switch>
-                {/* <Route path = '/' component = {} /> */}
+                <Route path = '/' component = { Index } />
             </Switch>
         </Router>
     </Provider>
