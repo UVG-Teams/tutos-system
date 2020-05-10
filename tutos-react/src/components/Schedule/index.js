ad772a91b769e945c06067632581e304b35e32d3
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-05-10 16:19:06 -0600

    Schedule

--- tutos-react/src/components/Schedule/index.js
+++ tutos-react/src/components/Schedule/index.js
@@ -9,30 +9,83 @@ const Location = ({state}) => {
         <div className="page">
             <div className="schedule_form">
                 <h2>¿Cuándo estás disponible para la tutoría?</h2>
-                <div className="morning">
+                <div className="days">
+                    <p className="day">Lun</p>
+                    <p className="day">Mar</p>
+                    <p className="day">Mie</p>
+                    <p className="day">Jue</p>
+                    <p className="day">Vie</p>
+                    <p className="day">Sab</p>
+                    <p className="day">Dom</p>
+                </div>
+                <div className="container">
                     <div className="labels">
-                        <p className="mainLabel">Mañana</p>
-                        <p>Antes de las 12 p.m.</p>
+                        <div>
+                            <p className="mainLabel">Mañana</p>
+                            <p>Antes de las 12 p.m.</p>
+                        </div>
+                        <div>
+                            <p className="mainLabel">Tarde</p>
+                            <p>12 - 5 p.m.</p>
+                        </div>
+                        <div>
+                            <p className="mainLabel">Noche</p>
+                            <p>Después de las 5 p.m.</p>
+                        </div>
                     </div>
-                    <div className="checkboxes">
-                        <input type="checkbox" value="mon"/>
-                        <input type="checkbox" value="tue"/>
-                        <input type="checkbox" value="wed"/>
-                        <input type="checkbox" value="thu"/>
-                        <input type="checkbox" value="fri"/>
-                        <input type="checkbox" value="sat"/>
-                        <input type="checkbox" value="sun"/>
+                    <div className="monday">
+                        <div className="checkboxes">
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                        </div>
+                    </div>
+                    <div className="tuesday">
+                        <div className="checkboxes">
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                        </div>
+                    </div>
+                    <div className="wednesday">
+                        <div className="checkboxes">
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                        </div>
+                    </div>
+                    <div className="thursday">
+                        <div className="checkboxes">
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                        </div>
+                    </div>
+                    <div className="friday">
+                        <div className="checkboxes">
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                        </div>
+                    </div>
+                    <div className="saturday">
+                        <div className="checkboxes">
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                        </div>
+                    </div>
+                    <div className="sunday">
+                        <div className="checkboxes">
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                            <input type="checkbox" value="mon" className="check"/>
+                        </div>
                     </div>
                 </div>
-
-                <div className="links">
-                    {/* TODO: poner links correctos */}
-                <Link to='/quick_recommendation/location'>
-                    <a className="back">Atrás</a>
-                </Link>
-                <Link to=''>
-                    <button type="button" className="next">Siguiente</button>
-                </Link>
+                <div className="free">
+                    <input type="radio" className="radioButton" value="free"/>
+                    <label for="free">En cualquier momento</label>
                 </div>
             </div>
         </div>
