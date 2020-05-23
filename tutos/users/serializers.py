530a01a1b80fb8993e81ec3a77e46dc209c2cdf3
FR98 <ros18676@uvg.edu.gt>
2020-05-22 18:53:55 -0600

    Add User viewset and serializer

--- tutos/users/serializers.py
+++ tutos/users/serializers.py
@@ -1,20 +1,16 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 from users.models import UserDetail
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetail
-        fields = (
-            'id',
-            'user',
-            'language',
-            'phone',
-            'gender',
-            'birthdate',
-            'location',
-            'is_tutor',
-            'institution',
-            'career',
-        )
+        fields = '__all__'
+
--- tutos/users/views.py
+++ tutos/users/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
+from django.contrib.auth.models import User
 from users.models import UserDetail
-from users.serializers import UserDetailSerializer
+from users.serializers import UserSerializer, UserDetailSerializer
 from permissions.services import APIPermissionClassFactory
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (
+        APIPermissionClassFactory(
+            name='UserPermission',
+            permission_configuration={
+                'base': {
+                    'create': True,
+                    'list': True,
+                },
+                'instance': {
+                    'retrieve': True,
+                    'update': True,
+                    'partial_update': True,
+                    'destroy': True,
+                }
+            }
+        ),
+    )
 
 
 class UserDetailViewSet(viewsets.ModelViewSet):
