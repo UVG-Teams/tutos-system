0519757e56a4f4d041e45566cdf6550731c9d2d1
FR98 <ros18676@uvg.edu.gt>
2020-05-23 14:15:24 -0600

    Add login sagas, token refresh, logged navbar, redux persist, middleware, configured store

--- tutos-react/src/components/Login/index.js
+++ tutos-react/src/components/Login/index.js
@@ -12,61 +12,68 @@ import {
 import * as selectors from '../../reducers';
 import * as actions from '../../actions/auth';
 
+import {
+    SuccessBtn,
+} from '../Buttons';
+
 import './styles.css'; 
-import '../../index.css';
-import '../../normalize.css';
 
 
-const Login = ({ onSubmit, isLoading }) => {
+const Login = ({
+    // onSubmit,
+    isLoading,
+    error = null,
+}) => {
     const [username, changeUsername] = useState('');
     const [password, changePassword] = useState('');
     return (
         <Popup  trigger={<button  className="popup">Login</button>} position="bottom center">
             <p>
-                <input className="input-login"
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={e => changeUsername(e.target.value)}
+                <input 
+                    className="input-login"
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => changeUsername(e.target.value)}
                 />
             </p>
             <p>
-                <input className="input-login"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={e => changePassword(e.target.value)}
+                <input 
+                    className="input-login"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => changePassword(e.target.value)}
                 />
             </p>
             <p>
                 {
-                isLoading ? (
-                    <strong>{'Cargando...'}</strong>
-                ) : (
-                    <Link to = 'dashboardTutor'>
-                        <button>
-                            Continuar
-                        </button>
-                        {/* <button className="continuar" type="submit" onClick={
-                        () => onSubmit(username, password)
-                        }>
-                        {'Continuar'}
-                        </button> */}
-                    </Link>
-                )
+                    isLoading ? (
+                        <strong>{'Cargando...'}</strong>
+                    ) : (
+                        <SuccessBtn text={'Login'} action={actions.startLogin(username, password)} />
+                    )
                 }
             </p>
+            {
+                error && (
+                <p>
+                    <strong className='error-text'>{ error }</strong>
+                </p>
+                )
+            }
         </Popup>
     );
 };
 
 export default connect(
-    (state) => ({
+    state => ({
         isLoading: selectors.getIsAuthenticating(state),
+        error: selectors.getAuthenticatingError(state),
     }),
-    dispatch => ({
-        onSubmit(username, password) {
-            dispatch(actions.startLogin(username, password));
-          },
-    }),
+    // dispatch => ({
+    //     onSubmit(username, password) {
+    //         dispatch(actions.startLogin(username, password));
+    //       },
+    // }),
 )(Login);
