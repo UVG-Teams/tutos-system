ec36b54c4a2c8399324e4c1c37b3737861a5098e
FR98 <ros18676@uvg.edu.gt>
2020-05-23 14:48:58 -0600

    Fix styles

--- tutos-react/src/components/Login/index.js
+++ tutos-react/src/components/Login/index.js
@@ -1,26 +1,15 @@
 import React, { useState, Fragment } from 'react';
 import { connect } from 'react-redux';
 import Popup from 'reactjs-popup';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useRouteMatch,
-    useParams
-} from "react-router-dom";
 import * as selectors from '../../reducers';
 import * as actions from '../../actions/auth';
 
-import {
-    SuccessBtn,
-} from '../Buttons';
+import { SuccessBtn } from '../Buttons';
 
 import './styles.css'; 
 
 
 const Login = ({
-    // onSubmit,
     isLoading,
     error = null,
 }) => {
@@ -71,9 +60,4 @@ export default connect(
         isLoading: selectors.getIsAuthenticating(state),
         error: selectors.getAuthenticatingError(state),
     }),
-    // dispatch => ({
-    //     onSubmit(username, password) {
-    //         dispatch(actions.startLogin(username, password));
-    //       },
-    // }),
 )(Login);
