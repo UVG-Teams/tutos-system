d682754bfe2dc124cebe07149687748bc22590a5
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2020-08-09 12:48:34 -0600

    Mi IP

--- tutos/tutos/settings.py
+++ tutos/tutos/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '%!yr-7v#+-x@zhir1xj9+%ic8qgkyx@8gl5!8sn5#w&=pjx(#y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.5']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.5','192.168.1.22']
 
 
 # Application definition
