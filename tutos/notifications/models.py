50cb48206576d1b72203c55870c0854f875c1ef0
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2020-10-19 09:12:11 -0600

    urlNotifications

---
+++ tutos/notifications/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Notification(models.Model):
+    title = models.CharField(
+        max_length = 50,
+		null = False,
+    )
+    description = models.CharField(
+        max_length = 200,
+		null = False,
+    )
+    date = models.DateTimeField(
+        auto_now = False,
+        auto_now_add = False,
+        null = False,
+    ) 
+    user = models.ForeignKey(
+        User,
+        null = False,
+        on_delete = models.CASCADE,
+    )
