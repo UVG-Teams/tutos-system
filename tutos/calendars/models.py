b2cef1c60abef77820ce73e50fe047e22e1e6dcb
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2020-11-03 15:16:39 -0600

    Eventos

--- tutos/calendars/models.py
+++ tutos/calendars/models.py
@@ -28,9 +28,14 @@ class Event(models.Model):
     date = models.DateTimeField(
         null = False,
     )
+    typeEvent = models.CharField(
+        max_length = 20,
+        null = False,
+    )
     created_at = models.DateTimeField(
         auto_now_add = True
     )
     updated_at = models.DateTimeField(
         auto_now = True
     )
+
